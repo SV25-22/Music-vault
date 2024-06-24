f4a2590597bbeca63f70ccb269017c523c185f91
Luka <luka.lukas159@gmail.com>
2024-06-24 16:55:56 +0200

    feature: adding the generic controller

---
+++ MusicVault/Backend/BuildingBlocks/Controller/GenericController.cs
@@ -0,0 +1,39 @@
+﻿using MusicVault.Backend.BuildingBlocks.Observer;
+using MusicVault.Backend.BuildingBlocks.Storage;
+using System.Collections.Generic;
+
+namespace MusicVault.Backend.BuildingBlocks.Controller;
+
+public abstract class GenericController<T, R> 
+    where T: IDAble
+    where R: SQLGenericRepository<T> {
+    private readonly R repository;
+    private readonly Subject Subject;
+
+    public GenericController(R repository, Subject subject) {
+        this.repository = repository;
+        Subject = subject;
+    }
+
+    public void Subscribe(IObserver observer) {
+        Subject.Subscribe(observer);
+    }
+
+    public void Add(T entity) {
+        repository.Add(entity);
+        Subject.NotifyObservers();
+    }
+
+    public void Update(T entity) {
+        repository.Update(entity);
+        Subject.NotifyObservers();
+    }
+
+    public T? Get(int id) {
+        return repository.Get(id);
+    }
+
+    public List<T> GetAll() { 
+        return repository.GetAll();
+    }
+}
