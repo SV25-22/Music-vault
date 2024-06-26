47b2f3ff0d0127bfcb1d23183217eb34ae00b4d4
Luka <luka.lukas159@gmail.com>
2024-06-26 23:51:07 +0200

    feature: adding system and email common services

---
+++ MusicVault/Backend/Common/Sistem.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicVault.Backend.Common {
+    internal class Sistem {
+        // treba da ima automatsko stopiranje glasanja
+        // treba da ima automatski top 10 za glasanje
+    }
+}
