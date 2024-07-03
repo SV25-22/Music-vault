c00d0dd629e8311c507cf994669cdb30c9e35c34
Momir Stanišić <momir.stanisic.ns@gmail.com>
2024-07-03 12:35:39 +0200

    feat: admin i common views

---
+++ MusicVault/Frontend/AdminView/AdminWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using MusicVault.Backend.BuildingBlocks.Observer;
+using MusicVault.Backend.Controllers;
+using System.Windows;
+
+namespace MusicVault.Frontend.AdminView;
+
+public partial class AdminWindow : Window, IObserver {
+    public readonly MuzickiSadrzajController muzickiSadrzajController;
+    public readonly RecenzijaController recenzijaController;
+    public readonly KorisnikController korisnikController;
+    public readonly IzvodjacController izvodjacController;
+    //public readonly GlasanjeController glasanjeController;
+    public readonly IzvodiController izvodiController;
+    //public readonly GlasController glasController;
+    public readonly ZanrController zanrController;
+
+    public AdminWindow(KorisnikController korisnikController, MuzickiSadrzajController muzickiSadrzajController, ZanrController zanrController,
+                       RecenzijaController recenzijaController, IzvodjacController izvodjacController, IzvodiController izvodiController) {
+        this.muzickiSadrzajController = muzickiSadrzajController;
+        this.recenzijaController = recenzijaController;
+        this.korisnikController = korisnikController;
+        this.izvodjacController = izvodjacController;
+        this.izvodiController = izvodiController;
+        this.zanrController = zanrController;
+        DataContext = this;
+
+        muzickiSadrzajController.Subscribe(this);
+        korisnikController.Subscribe(this);
+        zanrController.Subscribe(this);
+        InitializeComponent();
+    }
+
+    public void Update() {
+        urednikControl.Update();
+        contentControl.Update();
+    }
+}
---
+++ MusicVault/Frontend/AdminView/ContentView/ContentControl.xaml.cs
@@ -0,0 +1,95 @@
+﻿using MusicVault.Backend.BuildingBlocks.Observer;
+using MusicVault.Frontend.AdminView.ContentView;
+using MusicVault.Backend.Controllers;
+using System.Collections.ObjectModel;
+using MusicVault.Frontend.DTO;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows;
+using MusicVault.Frontend.AdminView.ContentView.AddViews;
+
+namespace MusicVault.Frontend.AdminView;
+
+public partial class ContentControl : UserControl, IObserver {
+    public static string[] TipoviSadrzaja { get; set; } = new string[] { "dela", "albumi", "nastupi", "izvođači" };
+    public ObservableCollection<SadrzajDTO> Sadrzaj { get; set; } = new();
+
+    private MuzickiSadrzajController muzickiSadrzajController;
+    private RecenzijaController recenzijaController;
+    private KorisnikController korisnikController;
+    private IzvodjacController izvodjacController;
+    private IzvodiController izvodiController;
+    private ZanrController zanrController;
+
+    public ContentControl() {
+        DataContext = this;
+        InitializeComponent();
+    }
+
+    private void OnControlLoaded(object sender, RoutedEventArgs e) {
+        AdminWindow? mainWindow = Window.GetWindow(this) as AdminWindow;
+        muzickiSadrzajController = mainWindow?.muzickiSadrzajController ?? new();
+        recenzijaController = mainWindow?.recenzijaController ?? new();
+        korisnikController = mainWindow?.korisnikController ?? new();
+        izvodjacController = mainWindow?.izvodjacController ?? new();
+        izvodiController = mainWindow?.izvodiController ?? new();
+        zanrController = mainWindow?.zanrController ?? new();
+        RefreshDataGrid();
+    }
+
+    private void TypeComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+        if (TypeComboBox.SelectedValue.ToString() == "dela")
+            AddContentBtn.Content = "Dodaj delo";
+        else if (TypeComboBox.SelectedValue.ToString() == "albumi")
+            AddContentBtn.Content = "Dodaj album";
+        else if (TypeComboBox.SelectedValue.ToString() == "nastupi")
+            AddContentBtn.Content = "Dodaj nastup";
+        else if (TypeComboBox.SelectedValue.ToString() == "izvođači")
+            AddContentBtn.Content = "Dodaj izvođača";
+        RefreshDataGrid();
+    }
+
+    private void AddZnrBtn_Click(object sender, RoutedEventArgs e) {
+        new AddZanrWindow(zanrController).Show();
+    }
+
+    private void AddContentBtn_Click(object sender, RoutedEventArgs e) {
+        if (TypeComboBox.SelectedValue.ToString() == "dela")
+            new AddTrackWindow().Show();
+        else if (TypeComboBox.SelectedValue.ToString() == "albumi")
+            new AddAlbumWindow().Show();
+        else if (TypeComboBox.SelectedValue.ToString() == "nastupi")
+            new AddNastupWindow().Show();
+        else if (TypeComboBox.SelectedValue.ToString() == "izvođači")
+            new AddArtistWindow(korisnikController, zanrController, izvodjacController).Show();
+    }
+
+    private void SadrzajDataGrid_MouseDoubleClick(object sender, MouseButtonEventArgs e) {
+        if (TypeComboBox.SelectedValue.ToString() == "dela")
+            new EditTrackWindow().Show();
+        else if (TypeComboBox.SelectedValue.ToString() == "albumi")
+            new EditAlbumWindow().Show();
+        else if (TypeComboBox.SelectedValue.ToString() == "nastupi")
+            new EditNastupWindow().Show();
+        else if (TypeComboBox.SelectedValue.ToString() == "izvođači")
+            new EditArtistWindow().Show();
+    }
+
+    private void SearchTxtBox_TextChanged(object sender, TextChangedEventArgs e) => RefreshDataGrid();
+
+    public void Update() => RefreshDataGrid();
+
+    public void RefreshDataGrid() {
+        Sadrzaj.Clear();
+        string search = SearchTxtBox.Text;
+
+        if (TypeComboBox.SelectedValue.ToString() == "dela")
+            muzickiSadrzajController?.GetDela(search).ForEach(delo => Sadrzaj.Add(new SadrzajDTO(delo)));
+        else if (TypeComboBox.SelectedValue.ToString() == "albumi")
+            muzickiSadrzajController?.GetAlbumi(search).ForEach(album => Sadrzaj.Add(new SadrzajDTO(album)));
+        else if (TypeComboBox.SelectedValue.ToString() == "nastupi")
+            muzickiSadrzajController?.GetNastupi(search).ForEach(nastup => Sadrzaj.Add(new SadrzajDTO(nastup)));
+        else if (TypeComboBox.SelectedValue.ToString() == "izvođači")
+            izvodjacController?.Search(search).ForEach(izvodjac => Sadrzaj.Add(new SadrzajDTO(izvodjac)));
+    }
+}
