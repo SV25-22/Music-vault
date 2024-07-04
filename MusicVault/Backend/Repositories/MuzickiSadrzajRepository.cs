e66e4b9e18ca9924205984a52b8ad776eaa5026d
Momir Stanišić <momir.stanisic.ns@gmail.com>
2024-07-04 08:52:04 +0200

    fix: added include kod get za više sadržaja

--- MusicVault/Backend/Repositories/MuzickiSadrzajRepository.cs
+++ MusicVault/Backend/Repositories/MuzickiSadrzajRepository.cs
@@ -9,17 +9,17 @@ namespace MusicVault.Backend.Repositories;
 public class MuzickiSadrzajRepository : SQLGenericRepository<MuzickiSadrzaj> {
     public static List<Delo> GetDela(string search = "") {
         using var context = new SqlDbContext();
-        return context.Delo.Where(d => string.IsNullOrEmpty(search) || d.Opis.ToLower().Contains(search.ToLower())).ToList();
+        return context.Delo.Include(d => d.MuzickiSadrzaji).Where(d => string.IsNullOrEmpty(search) || d.Opis.ToLower().Contains(search.ToLower())).ToList();
     }
 
     public static List<Album> GetAlbumi(string search = "") {
         using var context = new SqlDbContext();
-        return context.Album.Where(a => string.IsNullOrEmpty(search) || a.Opis.ToLower().Contains(search.ToLower())).ToList();
+        return context.Album.Include(a => a.MuzickiSadrzaji).Where(a => string.IsNullOrEmpty(search) || a.Opis.ToLower().Contains(search.ToLower())).ToList();
     }
 
     public static List<Nastup> GetNastupi(string search = "") {
         using var context = new SqlDbContext();
-        return context.Nastup.Where(n => string.IsNullOrEmpty(search) || n.Opis.ToLower().Contains(search.ToLower())).ToList();
+        return context.Nastup.Include(n => n.MuzickiSadrzaji).Where(n => string.IsNullOrEmpty(search) || n.Opis.ToLower().Contains(search.ToLower())).ToList();
     }
 
     public MuzickiSadrzaj DodajMuzickiSadrzaj(MuzickiSadrzaj entity) {
